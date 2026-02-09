14a20d28f85f610a0d0197736444f61f56808c18
GURU KARTHICK N S <gurukarthicksanty123@gmail.com>
2026-02-09 11:56:03 +0530

    Create 20-ValidParentheses

---
+++ 20-ValidParentheses/20-ValidParentheses.java
@@ -0,0 +1,22 @@
+// Last updated: 2/9/2026, 11:56:03 AM
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st=new Stack<>();
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(ch=='('||ch=='['||ch=='{'){
+                st.push(ch);
+            }else{
+                 if (st.isEmpty()) {
+                    return false;
+                }
+                if((st.peek()=='(' && ch==')')||(st.peek()=='{' && ch=='}')||(st.peek()=='[' && ch==']')||(st.peek()=='<' && ch=='>')){
+	                st.pop();
+	            }else{
+                    return false;
+                }
+            }
+        }
+        return(st.isEmpty());
+    }
+}
